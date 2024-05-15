95b9eaf18309072be2174b2a72881a3dccfb0416
yunuskyya <0yunuskaya0@gmail.com>
2024-05-15 20:08:00 +0300

    security confiq is done.

---
+++ src/main/java/com/yunuskaya/banka/security/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.yunuskaya.banka.security;
+
+import com.yunuskaya.banka.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final UserService userService;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(UserService userService, BCryptPasswordEncoder passwordEncoder, JwtAuthFilter jwtAuthFilter) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x ->
+                        x.requestMatchers("/auth/account/depositc**", "/auth/addNewUser/**", "/auth/generateToken/**").permitAll()
+                )
+                .authorizeHttpRequests(x ->
+                        x.requestMatchers("/auth/user").hasRole("USER")
+                                .requestMatchers("/auth/admin").hasRole("ADMIN")
+                )
+
+                .sessionManagement(x -> x.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+
+    }
+
+}
